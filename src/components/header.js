dad4ffff356e3699e82263ba336660ac7175c3bc
differs <keepmysterious@tutanota.com>
2023-06-11 14:31:21 +0800

    .

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Dropdown} from 'react-bootstrap';
+import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import LOGO from '../assets/images/LOGO2.png';
+
 
 function AppHeader() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Corporate</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={LOGO} width={25} height={25}  />
+          
+          Origin of Civilization
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -19,6 +29,16 @@ function AppHeader() {
             <Nav.Link href="#pricing">Pricing</Nav.Link>
             <Nav.Link href="#blog">Blog</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
+            <Dropdown >
+                <DropdownToggle >Docs</DropdownToggle>
+                <DropdownMenu >
+                  <DropdownItem href="#">Learn React</DropdownItem>
+                  <DropdownItem href="#">Documentation</DropdownItem>
+                  <DropdownItem href="#">Tutorials</DropdownItem>
+                  <DropdownItem href="#">Courses</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/hero.js
+++ src/components/hero.js
@@ -3,23 +3,23 @@ import Carousel from 'react-bootstrap/Carousel';
 var heroData = [
   {
     id: 1,
-    image: require('../assets/images/img-hero1.jpg'),
-    title: 'The perfect design for your website',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!',
-    link: 'https://www.google.com'
+    image: require('../assets/images/hero2.png'),
+    title: 'Start an interesting GameFi journey',
+    description: 'Register in our DAPP in advance, and get the priority experience right and free novice gift package first! Before the game starts, form your forces and always be one step ahead!',
+    link: 'https://etherscan.io'
   },
   {
     id: 2,
-    image: require('../assets/images/img-hero2.jpg'),
-    title: 'Start Your Future Financial Plan',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!',
+    image: require('../assets/images/hero2.png'),
+    title: 'Start an interesting GameFi journey',
+    description: 'Register in our DAPP in advance, and get the priority experience right and free novice gift package first! Before the game starts, form your forces and always be one step ahead!',
     link: 'https://www.facebook.com'
   },
   {
     id: 3,
-    image: require('../assets/images/img-hero3.jpg'),
-    title: 'Enjoy the Difference',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!',
+    image: require('../assets/images/hero2.png'),
+    title: 'Start an interesting GameFi journey',
+    description: 'Register in our DAPP in advance, and get the priority experience right and free novice gift package first! Before the game starts, form your forces and always be one step ahead!',
     link: 'https://www.twitter.com'
   }
 ]
@@ -36,11 +36,13 @@ function AppHero() {
                     className="d-block w-100"
                     src={hero.image}
                     alt={"slide " + hero.id}
+                    // width={1024}
+                    // height={1024}
                   />
                   <Carousel.Caption>
                     <h2>{hero.title}</h2>
                     <p>{hero.description}</p>
-                    <a className="btn btn-primary" href={hero.link}>Learn More <i className="fas fa-chevron-right"></i></a>
+                    <a className="btn btn-primary" href={hero.link}>Register Now <i className="fas fa-chevron-right"></i></a>
                   </Carousel.Caption>             
                 </Carousel.Item>
               );
