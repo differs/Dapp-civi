035bcaeaf83ac3d6ae15b7949a2d062dbc382700
differs <keepmysterious@tutanota.com>
2023-06-25 21:55:04 +0800

    social

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,17 +27,11 @@ function AppFooter() {
       <div className="socials">
         <ul>
         {/* <FontAwesomeIcon icon="fa-brands fa-github" spin style={{color: "#041025",}} /> */}
-          <li><a href="https://github.com"><i className="fab fa-github"></i></a></li>
-          <li><a href="https://github.com"><i className="fab fa-youtube"></i></a></li>
-          <li><a href="https://github.com"><i className="fab fa-instagram"></i></a></li>
+          <li><a href="https://github.com/differs/hackathon-2023-summer"><i className="fab fa-github"></i></a></li>
+          <li><a href="https://www.youtube.com/@OriginofCivilization/playlists"><i className="fab fa-youtube"></i></a></li>
+          <li><a href="https://github.com"><i class="fa-brands fa-weixin"></i></a></li>
           <li><a href="https://github.com"><i className="fab fa-telegram"></i></a></li>
-
-
-
-
-          {/* <li><a href="https://www.facebook.com"><i className="fab fa-facebook-f"></i></a></li> */}
-          <li><a href="https://www.twitter.com"><i className="fab fa-twitter"></i></a></li>
-          {/* <li><a href="https://www.linkedin.com"><i className="fab fa-linkedin-in"></i></a></li> */}
+          <li><a href="https://twitter.com/OriginOfCivi"><i className="fab fa-twitter"></i></a></li>
         </ul>
       </div>
       {
